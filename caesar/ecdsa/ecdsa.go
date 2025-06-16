9628c44e7eaed53d77a192ba9a52c3988e3a4877
sato yoshiyuki <yoshi.389111@gmail.com>
2025-06-16 17:52:34 +0900

    :recycle: refact: update ECDSA signing and verification methods for compatibility with ASN.1 encoding

--- caesar/ecdsa/ecdsa.go
+++ caesar/ecdsa/ecdsa.go
@@ -4,9 +4,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/rand"
 	"crypto/sha256"
-	"encoding/asn1"
 	"fmt"
-	"math/big"
 
 	"github.com/yoshi389111/git-caesar/caesar/aes"
 )
@@ -47,21 +45,12 @@ func Decrypt(prvKey *ecdsa.PrivateKey, peersPubKey *ecdsa.PublicKey, ciphertext 
 	return aes.Decrypt(sharedKey[:], ciphertext)
 }
 
-// sigParam is used for ASN.1 encoding/decoding of ECDSA signatures.
-type sigParam struct {
-	R, S *big.Int
-}
-
 // Sign creates an ECDSA signature for the given message.
 func Sign(prvKey *ecdsa.PrivateKey, message []byte) ([]byte, error) {
 	hash := sha256.Sum256(message)
-	r, s, err := ecdsa.Sign(rand.Reader, prvKey, hash[:])
+	sig, err := ecdsa.SignASN1(rand.Reader, prvKey, hash[:])
 	if err != nil {
 		return nil, fmt.Errorf("failed to sign ecdsa: %w", err)
-	}
-	sig, err := asn1.Marshal(sigParam{R: r, S: s})
-	if err != nil {
-		return nil, fmt.Errorf("failed to ecdsa signature marshalling: %w", err)
 	}
 	return sig, nil
 }
@@ -69,10 +58,5 @@ func Sign(prvKey *ecdsa.PrivateKey, message []byte) ([]byte, error) {
 // Verify checks an ECDSA signature for the given message.
 func Verify(pubKey *ecdsa.PublicKey, message, sig []byte) bool {
 	hash := sha256.Sum256(message)
-	signature := &sigParam{}
-	_, err := asn1.Unmarshal(sig, signature)
-	if err != nil {
-		return false
-	}
-	return ecdsa.Verify(pubKey, hash[:], signature.R, signature.S)
+	return ecdsa.VerifyASN1(pubKey, hash[:], sig)
 }
