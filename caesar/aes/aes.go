2d8530d22ea6a4ac81134ce9b65e4c66e530933b
sato yoshiyuki <yoshi.389111@gmail.com>
2025-06-18 20:25:19 +0900

    :sparkles: feat: add AES-GCM encryption and decryption support for version 2

--- caesar/aes/aes.go
+++ caesar/aes/aes.go
@@ -6,21 +6,24 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"fmt"
+	"io"
 
 	"github.com/yoshi389111/git-caesar/caesar/common"
 )
 
-// Encrypt encrypts a message using AES-256.
+// Encrypt encrypts a message using AES.
 func Encrypt(version string, key, plaintext []byte) ([]byte, error) {
 	switch version {
 	case common.Version1:
 		return encryptV1(key, plaintext)
+	case common.Version2:
+		return encryptV2(key, plaintext)
 	default:
 		return nil, fmt.Errorf("unknown `caesar.json` version `%s`", version)
 	}
 }
 
-// Encrypt encrypts a message using AES-256-CBC with PKCS#7 padding.
+// Encrypt encrypts a message using AES-CBC with PKCS#7 padding.
 func encryptV1(key, plaintext []byte) ([]byte, error) {
 
 	// pad the message with PKCS#7
@@ -48,17 +51,43 @@ func encryptV1(key, plaintext []byte) ([]byte, error) {
 	return ciphertext, nil
 }
 
-// Decrypt decrypts a message using AES-256.
+// Encrypt encrypts a message using AES-GCM.
+func encryptV2(key, plaintext []byte) ([]byte, error) {
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create AES cipher block for encryption: %w", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create GCM mode: %w", err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, fmt.Errorf("failed to generate nonce: %w", err)
+	}
+
+	ciphertext := gcm.Seal(nil, nonce, plaintext, nil)
+	ciphertext = append(nonce, ciphertext...)
+
+	return ciphertext, nil
+}
+
+// Decrypt decrypts a message using AES.
 func Decrypt(version string, key, ciphertext []byte) ([]byte, error) {
 	switch version {
 	case common.Version1:
 		return decryptV1(key, ciphertext)
+	case common.Version2:
+		return decryptV2(key, ciphertext)
 	default:
 		return nil, fmt.Errorf("unknown `caesar.json` version `%s`", version)
 	}
 }
 
-// Decrypt decrypts a message using AES-256-CBC with PKCS#7 padding.
+// Decrypt decrypts a message using AES-CBC with PKCS#7 padding.
 func decryptV1(key, ciphertext []byte) ([]byte, error) {
 
 	// Check if ciphertext is long enough to contain an IV
@@ -100,5 +129,34 @@ func decryptV1(key, ciphertext []byte) ([]byte, error) {
 		}
 	}
 	plaintext := decMsg[:msgLen-padding]
+	return plaintext, nil
+}
+
+// Decrypt decrypts a message using AES-GCM.
+func decryptV2(key, ciphertext []byte) ([]byte, error) {
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create AES cipher block for encryption: %w", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create GCM mode: %w", err)
+	}
+
+	if len(ciphertext) < gcm.NonceSize() {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	nonce := ciphertext[:gcm.NonceSize()]
+	encMsg := ciphertext[gcm.NonceSize():]
+
+	// Decrypt the ciphertext
+	plaintext, err := gcm.Open(nil, nonce, encMsg, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt ciphertext: %w", err)
+	}
+
 	return plaintext, nil
 }
